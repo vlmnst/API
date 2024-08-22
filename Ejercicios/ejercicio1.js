04764fc6426fa5de23d75a5300db98249607a078
vlmnst <vale-mansueto@hotmail.com>
2024-08-22 12:49:59 -0300

    muevo los archivos y elimino los innecesarios

--- Ejercicios/ejercicio1.js
+++
@@ -1,6 +0,0 @@
-//1. Crear un algoritmo que muestre los números impares entre el 0 y el 100.
-let i = 0;
-while ( i < 101 ){
-    if ( i % 2 === 1) console.log(i);
-    i++
-}
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,12 +3,10 @@ import { Router } from "express";
 import { AuthController } from "../controllers";
 import { authValidator } from "../validators";
 import { asyncHandler } from "../middlewares/handlers.middleware";
-// import { checkAuth } from "../middlewares";
 
 const router = Router();
 
 router.post( "/login", authValidator.login, asyncHandler( AuthController.login ) );
 
-// router.get( "/logout", [checkAuth] , asyncHandler( AuthController.logout ) );
 
 export default router;
